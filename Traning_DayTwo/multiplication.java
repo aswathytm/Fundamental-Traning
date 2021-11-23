764632badd8b20635db1def0a9519efafeabdd48
Aswathy T M <60748812+aswathytm@users.noreply.github.com>
2021-11-23 18:21:56 +0530

    Update multiplication.java

--- Traning_DayTwo/multiplication.java
+++ Traning_DayTwo/multiplication.java
@@ -1,11 +1,16 @@
 import java.util.*;
-public class multiplication{
+public class Main{
     public static void main(String[]args){
+        int multiplicationTable[]=new int[10];
         Scanner in=new Scanner(System.in);
-        System.out.println("Enter a number:");
+        System.out.print("Enter a number:");
         int number=in.nextInt();
-        for(int i=1;i<=10;i++){
-            System.out.println(number + " * " + (i) + " = " +(number*(i)));
+        for(int i=0;i<10;i++){
+            multiplicationTable[i]=number*(i+1);
+        }
+        for(int j=0;j<10;j++)
+        {
+            System.out.println(j+1 + " * " + number + " = " + multiplicationTable[j]);
         }
     }
 }
